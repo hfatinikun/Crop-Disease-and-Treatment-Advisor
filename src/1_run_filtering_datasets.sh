5cce46f0962af7514f21fd93e0283b95a79b62d4
Amo Ikiror <amoikiror@gmail.com>
2026-05-07 23:28:02 +0200

    update file name

---
+++ src/1_run_filtering_datasets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=filter
+#SBATCH --partition=pibu_el8
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G 
+#SBATCH --time=4:00:00
+#SBATCH --output=/data/users/aikiror/deepLearning/project/logFiles/1_run_filtering_datasets/output_%J.o
+#SBATCH --error=/data/users/aikiror/deepLearning/project/logFiles/1_run_filtering_datasets/error_%J.e
+#SBATCH --mail-user=amo.ikiror@students.unibe.ch
+#SBATCH --mail-type=begin,end,fail
+
+#dir and paths
+WORKDIR="/data/users/aikiror/deepLearning/project"
+SCRIPT_PY="${WORKDIR}/src/filtering_datasets.py"
+ENVIRONMENT="${WORKDIR}/tools/envs/dl_project/bin/activate"
+
+cd ${WORKDIR}
+
+#load pytorch
+module load PyTorch/1.10.0-foss-2021a-CUDA-11.3.1
+
+#activate environment 
+source ${ENVIRONMENT}
+
+#run code
+python ${SCRIPT_PY}
---
+++ src/2_run_alt_data_loader.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=load
+#SBATCH --partition=pibu_el8
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G 
+#SBATCH --time=4:00:00
+#SBATCH --output=/data/users/aikiror/deepLearning/project/logFiles/2_run_alt_data_loader/output_%J.o
+#SBATCH --error=/data/users/aikiror/deepLearning/project/logFiles/2_run_alt_data_loader/error_%J.e
+#SBATCH --mail-user=amo.ikiror@students.unibe.ch
+#SBATCH --mail-type=begin,end,fail
+
+#dir and paths
+WORKDIR="/data/users/aikiror/deepLearning/project"
+SCRIPT_PY="${WORKDIR}/src/alt_data_loader.py"
+ENVIRONMENT="${WORKDIR}/tools/envs/dl_project/bin/activate"
+
+cd ${WORKDIR}
+
+#load pytorch
+module load PyTorch/1.10.0-foss-2021a-CUDA-11.3.1
+
+#activate environment 
+source ${ENVIRONMENT}
+
+#run code
+python ${SCRIPT_PY}
