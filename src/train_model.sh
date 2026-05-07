368b7401a693239885bd1a2f9c6d60253a2a25b5
Amo Ikiror <amoikiror@gmail.com>
2026-05-07 09:31:15 +0200

    initial commit

---
+++ src/train_model.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=train_model
+#SBATCH --partition=pgpu
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem=16G 
+#SBATCH --time=4:00:00
+#SBATCH --output=/data/users/aikiror/deepLearning/project/logFiles/train_model/output_%J.o
+#SBATCH --error=/data/users/aikiror/deepLearning/project/logFiles/train_model/error_%J.e
+#SBATCH --mail-user=amo.ikiror@students.unibe.ch
+#SBATCH --mail-type=begin,end,fail
+
+
+#utilizing pgpu partition as it has gpu and pibu-el8 doesnt.
+#also changed cpus-per-gpu
+
+WORKDIR="/data/users/aikiror/deepLearning/project"
+TRAIN_PY=""
+ENVIRONMENT=""
+
+#activate environment
+source activate ${ENVIRONMENT}
+
+#run training code
+python train.py
